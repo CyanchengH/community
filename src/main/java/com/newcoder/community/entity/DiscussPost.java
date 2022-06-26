73976c20367bc461423332cbb1e1ded027428f4e
fogland <691161213@qq.com>
2022-06-26 14:11:07 +0800

    第六章完成

--- src/main/java/com/newcoder/community/entity/DiscussPost.java
+++ src/main/java/com/newcoder/community/entity/DiscussPost.java
@@ -1,21 +1,41 @@
 package com.newcoder.community.entity;
 
+import org.apache.ibatis.annotations.Insert;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.Date;
 
 
-//@Document(indexName = "", type = "", shards = "", replicas = )
+@Document(indexName = "discusspost", type = "_doc", shards = 6, replicas = 3)
 public class DiscussPost {
 
+    @Id
     private int id;
+    @Field(type = FieldType.Integer)
     private int userId;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")// 解析器
     private String title;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;
+
+    @Field(type = FieldType.Integer)
     private int type;
+
+    @Field(type = FieldType.Integer)
     private int status;
+
+    @Field(type = FieldType.Date)
     private Date createTime;
+
+    @Field(type = FieldType.Integer)
     private int commentCount;
+
+    @Field(type = FieldType.Double)
     private double score;
 
     public int getId() {
